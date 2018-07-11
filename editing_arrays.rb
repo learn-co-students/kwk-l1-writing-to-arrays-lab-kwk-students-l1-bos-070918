1b7a2f710ad6ab2f3beea7b379c4c25dbc7ee1ac
Celine Anyaegbunam <cea03@icloud.com>
2018-07-11 17:51:57 +0000

    Done.

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,4 +1,4 @@
-rainbow_colors = [
+$rainbow_colors = [
   "yellow",
   "default",
   "light_cyan"
@@ -6,26 +6,16 @@ rainbow_colors = [
 # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
 
 def change_rainbow_colors
-  rainbow_colors[0] = "red"
-  rainbow_colors[1] = "light_red"
-  rainbow_colors[2] = "green"
-  rainbow_colors[3] = "blue"
-  puts rainbow_colors 
-  rainbow_colors = change_rainbow_colors
-  puts rainbow_colors 
-  end
+  $rainbow_colors[0..2] = ["red","light_red","light_yellow"]
 end 
-puts rainbow_colors 
 
-
-# def change_rainbow_colors
-#   $rainbow_colors[0..2] = ["red","light_red","light_yellow"]
-# end 
-
-# def add_colors
-#   $rainbow_colors.push("green","blue")
-# end
+def add_colors
+  $rainbow_colors.push("green","blue")
+end
  
+ change_rainbow_colors
+ add_colors
+ puts $rainbow_colors 
  
 # #   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 # # end
